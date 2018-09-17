f1991188ef1864dc9ff6180d0f7a9baf05a95cef
Zac Willmington <zac.willmington@gmail.com>
2018-09-17 18:45:30 +0000

    Done.

---
+++ index.js
@@ -0,0 +1,33 @@
+function getFirstSelector(selector){
+        return document.querySelector(selector);
+}
+
+function nestedTarget(){
+    return document.querySelector('#nested .target');
+}
+
+function deepestChild(){
+    return document.querySelector('#grand-node div div div div');
+}
+
+function increaseRankBy(n){
+
+
+    const rankedLists = document.querySelectorAll('.ranked-list')
+    const firstList = rankedLists[0]
+    const secondList = rankedLists[1]
+
+    let childElementsOfFirstList = firstList.children
+    let childElementsOfSecondList = secondList.children
+
+    console.log(childElementsOfFirstList);
+    for(let i = 0; i < childElementsOfFirstList.length; i++){
+             childElementsOfFirstList[i].innerHTML = parseInt(childElementsOfFirstList[i].innerHTML) + 3;
+        };
+
+        for(let i = 0; i < childElementsOfSecondList.length; i++){
+                 childElementsOfSecondList[i].innerHTML = parseInt(childElementsOfSecondList[i].innerHTML) + 3; 
+            };
+
+
+}
