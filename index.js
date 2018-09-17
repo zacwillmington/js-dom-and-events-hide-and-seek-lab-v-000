c3daa68e6a868ec6e0dbf08a05a1f78fa7e6000d
Zac Willmington <zac.willmington@gmail.com>
2018-09-17 18:05:44 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -7,6 +7,6 @@ function nestedTarget(){
 }
 
 function deepestChild(){
-    const grandNode = document.querySelector('grand-node');
+    const grandNode = document.querySelector('.grand-node');
     return grandNode[0];
 }
